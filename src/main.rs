a77401f219af3f2dca1bfcada1c352a5aa44a272
ElecBillYoutubeTutorial <varun.ultimate76@gmail.com>
2023-06-19 19:25:33 +0530

    Should be the final commit, although the user can enter numbers less than 1 and greater than 100, I don't know how to stop that yet. So, I'm leaving it like this

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,30 @@ fn main() {
     println!("Guess the number!");
     println!("Enter your guess (Number b/w 1 and 100)");    
     
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read guess");
-
-    let guess: u32 = guess.trim().parse().expect("Type a number from 1 to 100 please");
-
-    println!("You guessed : {guess} ");
-
-    match guess.cmp(&secret_number) {
-       Ordering::Less => println!("Guess higher!"),
-       Ordering::Greater => println!("Guess lower!"),
-       Ordering::Equal => println!("That's right!!"),
-    }
-
+   loop{
+    
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read guess");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("A number between 1 and 100 please");
+                continue;
+            } 
+        };
+    
+        println!("You guessed : {guess} ");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Guess higher!"),
+            Ordering::Greater => println!("Guess lower!"),
+            Ordering::Equal => {
+               println!("That's right!!");
+               break; 
+            }
+        } 
+    } 
 }
